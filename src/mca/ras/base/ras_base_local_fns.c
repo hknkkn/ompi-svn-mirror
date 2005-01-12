9c8b6d8ade3fb6b7dcc78b3a49ca396c6d91f6a8
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2005-01-12 01:12:40 +0000

    next

---
+++ src/mca/ras/base/ras_base_local_fns.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+/** @file:
+ *
+ */
+
+#include "orte_config.h"
+
+#include "include/orte_constants.h"
+
+#include "mca/orte_ras/base/base.h"
+
+
+/*
+ * "not available" functions
+ */
+int
+mca_orte_ras_base_allocate_not_available(void)
+{
+    return ORTE_ERR_UNREACH;
+}
+
+int
+mca_orte_ras_base_deallocate_not_available(void)
+{
+    return ORTE_ERR_UNREACH;
+}
