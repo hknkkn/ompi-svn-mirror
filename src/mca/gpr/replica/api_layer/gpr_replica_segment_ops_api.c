2290a5bcae0151dd5adf627d838af8160a565977
Ralph Castain <rhc@open-mpi-git-mirror.example.com>
2005-01-22 01:50:46 +0000

    Checkpoint revised gpr layout

---
+++ src/mca/gpr/replica/api_layer/gpr_replica_segment_ops_api.c
@@ -0,0 +1,34 @@
+/* -*- C -*-
+ *
+ * Copyright (c) 2004-2005 The Trustees of Indiana University.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+/** @file:
+ *
+ * The Open MPI General Purpose Registry - Replica component
+ *
+ */
+
+/*
+ * includes
+ */
+#include "orte_config.h"
+
+#include "include/orte_constants.h"
+
+
+#include "gpr_replica_api.h"
+
+int orte_gpr_replica_preallocate_segment(char *name, int num_slots)
+{
+    return ORTE_ERR_NOT_IMPLEMENTED;
+}
