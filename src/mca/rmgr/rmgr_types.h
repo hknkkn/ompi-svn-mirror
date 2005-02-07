0e7ae85e23cd7ff757e3b4bd1eb9430071fa9b41
Tim Woodall <twoodall@open-mpi-git-mirror.example.com>
2005-02-07 18:48:43 +0000

    combine info/context

--- src/mca/rmgr/rmgr_types.h
+++ src/mca/rmgr/rmgr_types.h
@@ -17,19 +17,13 @@
 
 typedef struct {
     ompi_object_t super;
-    char *application;
+    char   *app;
     int32_t num_procs;
-} orte_rmgr_app_info_t;
-
-OBJ_CLASS_DECLARATION(orte_rmgr_app_info_t);
-
-
-typedef struct {
-    ompi_object_t super;
     int32_t argc;
-    char **argv;
-    int32_t num_enviro;
-    char **enviro;
+    char  **argv;
+    int32_t num_env;
+    char  **env;
+    char   *cwd;
 } orte_rmgr_app_context_t;
 
 OBJ_CLASS_DECLARATION(orte_rmgr_app_context_t);
@@ -38,7 +32,6 @@ OBJ_CLASS_DECLARATION(orte_rmgr_app_context_t);
 /*
  * REGISTRY KEY NAMES FOR COMMON DATA
  */
-#define ORTE_RMGR_APP_INFO      "orte-rmgr-app-info"
 #define ORTE_RMGR_APP_CONTEXT   "orte-rmgr-app-context"
 #define ORTE_RMGR_APP_LOC       "orte-rmgr-app-location"
 #define ORTE_RMGR_LAUNCHER      "orte-rmgr-launcher"
