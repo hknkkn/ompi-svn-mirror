9c3bea2bf9b1d393434db4619ca1ec9865df2863
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2005-02-09 13:45:39 +0000

    Minor comment change to match the code

--- src/mca/rmaps/rmaps.h
+++ src/mca/rmaps/rmaps.h
@@ -100,9 +100,8 @@ typedef struct orte_rmaps_base_component_1_0_0_t orte_rmaps_base_component_1_0_0
 typedef orte_rmaps_base_component_1_0_0_t orte_rmaps_base_component_t;
 
 
-
 /*
- * Macro for use in components that are of type ns v1.0.0
+ * Macro for use in components that are of type rmaps v1.0.0
  */
 #define MCA_ORTE_RMAPS_BASE_VERSION_1_0_0 \
   /* ras v1.0 is chained to MCA v1.0 */ \
