4929b71ea34c0f09783c171d4500f1a325ca2052
Tim Woodall <twoodall@open-mpi-git-mirror.example.com>
2005-02-25 15:41:38 +0000

    add support for orte_app_context_t

--- src/mca/gpr/replica/functional_layer/gpr_replica_segment_fn.c
+++ src/mca/gpr/replica/functional_layer/gpr_replica_segment_fn.c
@@ -24,7 +24,7 @@
 #include "orte_config.h"
 
 #include "util/output.h"
-
+#include "util/argv.h"
 #include "mca/errmgr/errmgr.h"
 #include "mca/gpr/replica/transition_layer/gpr_replica_tl.h"
 
@@ -283,6 +283,25 @@ int orte_gpr_replica_xfer_payload(orte_gpr_value_union_t *dest,
             dest->exit_code = src->exit_code;
             break;
             
+        case ORTE_APP_CONTEXT:
+            if(NULL == src->app_context) {
+                dest->app_context = NULL;
+                break;
+            }
+            dest->app_context = OBJ_NEW(orte_app_context_t);
+            dest->app_context->idx = src->app_context->idx;
+            if(NULL != src->app_context->app) {
+                dest->app_context->app = strdup(src->app_context->app);
+            }
+            dest->app_context->num_procs = src->app_context->num_procs;
+            dest->app_context->argc = src->app_context->argc;
+            dest->app_context->argv = ompi_argv_copy(src->app_context->argv);
+            dest->app_context->num_env = src->app_context->num_env;
+            dest->app_context->env = ompi_argv_copy(src->app_context->env);
+            if(NULL != src->app_context->cwd) {
+                dest->app_context->cwd = strdup(src->app_context->cwd);
+            }
+            break;
         default:
             return ORTE_ERR_BAD_PARAM;
             break;
