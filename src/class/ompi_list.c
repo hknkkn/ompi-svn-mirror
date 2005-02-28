62f461119f6549aba897a509346caeb324d2c02d
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2005-02-28 20:38:27 +0000

    - Fix the prototype for the ompi_list_sort() compare function (needed   one more * from the last fix) - Add doxy docs for the compare function and the ompi_list_sort()   function so that no one else makes the mistakes I made when writing   compare functions ;-)

--- src/class/ompi_list.c
+++ src/class/ompi_list.c
@@ -197,22 +197,25 @@ int ompi_list_sort(ompi_list_t* list, ompi_list_item_compare_fn_t compare)
     ompi_list_item_t** items;
     size_t i, index=0;
 
-    if(list->ompi_list_length == 0)
+    if (0 == list->ompi_list_length) {
         return OMPI_SUCCESS;
-    items = (ompi_list_item_t**)malloc(sizeof(ompi_list_item_t*) * list->ompi_list_length);
-    if(NULL == items)
+    }
+    items = (ompi_list_item_t**)malloc(sizeof(ompi_list_item_t*) * 
+                                       list->ompi_list_length);
+
+    if (NULL == items) {
         return OMPI_ERR_OUT_OF_RESOURCE;
+    }
 
     while(NULL != (item = ompi_list_remove_first(list))) {
         items[index++] = item;
     }
     
-    qsort(items, list->ompi_list_length, sizeof(ompi_list_item_t*), (int(*)(const void*,const void*))compare);
-    for(i=0; i<index; i++)
+    qsort(items, index, sizeof(ompi_list_item_t*), 
+          (int(*)(const void*,const void*))compare);
+    for (i=0; i<index; i++) {
         ompi_list_append(list,items[i]);
+    }
     free(items);
     return OMPI_SUCCESS;
 }
-
-                                                                                                            
-
