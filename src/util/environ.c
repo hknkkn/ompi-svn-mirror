040944487eee194581bac0956e460b91079f2967
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2005-03-01 20:15:04 +0000

    Add a utility to merge environments and resolve conflicts

---
+++ src/util/environ.c
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+#include "ompi_config.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "include/constants.h"
+#include "util/argv.h"
+#include "util/environ.h"
+
+
+/*
+ * Merge two environ-like char arrays, ensuring that there are no
+ * duplicate entires
+ */
+char **ompi_environ_merge(char **minor, char **major)
+{
+    int argc = 0;
+    char **argv = NULL;
+    char *a, *b;
+    int i, j;
+
+    /* First, copy major */
+
+    argv = ompi_argv_copy(major);
+    argc = ompi_argv_count(argv);
+
+    /* Now go through minor and see if there's any non-duplicated env
+       variables in there that we can append to the new argv.  This is
+       not terribly efficient O(N*M), but this will never be in a
+       performance-critical path. */
+
+    for (i = 0; NULL != minor[i]; ++i) {
+        a = strchr(minor[i], '=');
+        if (NULL != a) {
+            *a = '\0';
+        }
+
+        for (j = 0; NULL != major[j]; ++j) {
+            b = strchr(major[j], '=');
+            if (NULL != b) {
+                *b = '\0';
+            }
+
+            if (0 == strcmp(a, b)) {
+                if (NULL != b) {
+                    *b = '=';
+                }
+                break;
+            }
+            if (NULL != b) {
+                *b = '=';
+            }
+        }
+        if (NULL != a) {
+            *a = '=';
+        }
+
+        /* Did we find a match in major?  If not, add this entry from
+           minor into the output argv */
+
+        if (NULL == major[j]) {
+            ompi_argv_append(&argc, &argv, minor[i]);
+        }
+    }
+
+    /* All done */
+
+    return OMPI_SUCCESS;
+}
---
+++ src/util/environ.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2004-2005 The Trustees of Indiana University.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 The Trustees of the University of Tennessee.
+ *                         All rights reserved.
+ * Copyright (c) 2004-2005 High Performance Computing Center Stuttgart, 
+ *                         University of Stuttgart.  All rights reserved.
+ * $COPYRIGHT$
+ * 
+ * Additional copyrights may follow
+ * 
+ * $HEADER$
+ */
+
+/**
+ * @file
+ *
+ * Generic helper routines for environment manipulation.
+ */
+
+#ifndef OMPI_ENVIRON_H
+#define OMPI_ENVIRON_H
+
+#include "ompi_config.h"
+
+#if defined(c_plusplus) || defined(__cplusplus)
+extern "C" {
+#endif
+    /**
+     * Merge two environ-like arrays into a single, new array, ensuring
+     * that there are no duplicate entries.
+     *
+     * @param minor Set 1 of the environ's to merge
+     * @param major Set 2 of the environ's to merge
+     * @retval New array of environ
+     *
+     * Merge two environ-like arrays into a single, new array,
+     * ensuring that there are no duplicate entires.  If there are
+     * duplicates, entries in the \em major array are favored over
+     * those in the \em minor array.
+     *
+     * Both \em major and \em minor are expected to be argv-style
+     * arrays (i.e., terminated with a NULL pointer).
+     *
+     * The array that is returned is an unencumbered array that should
+     * later be freed with a call to ompi_argv_free().
+     */
+    OMPI_DECLSPEC char **ompi_environ_merge(char **minor, char **major);
+
+#if defined(c_plusplus) || defined(__cplusplus)
+}
+#endif
+
+#endif /* OMPI_ARGV_H */
