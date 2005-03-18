743d91af5cb38ca168fbdd486295f6b7a61e959b
Jeff Squyres <jsquyres@open-mpi-git-mirror.example.com>
2005-03-18 15:29:54 +0000

    Add in --machinefile and -machinefile to be compatible with other MPI implementations.

--- src/tools/orterun/orterun.c
+++ src/tools/orterun/orterun.c
@@ -114,7 +114,10 @@ ompi_cmd_line_init_t cmd_line_init[] = {
       "Number of processes to run" },
 
     /* Set a hostfile */
-    { "rds", "hostfile", "path", '\0', NULL, "hostfile", 1,
+    { "rds", "hostfile", "path", '\0', "hostfile", "hostfile", 1,
+      NULL, OMPI_CMD_LINE_TYPE_STRING,
+      "Provide a hostfile" },
+    { "rds", "hostfile", "path", '\0', "machinefile", "machinefile", 1,
       NULL, OMPI_CMD_LINE_TYPE_STRING,
       "Provide a hostfile" },
 
